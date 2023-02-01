f33e28dc70cd253e9cc2d18d350daee88f860fc0
linda <yuu.lindq@gmail.com>
2023-02-01 14:43:01 +0900

    単純無向グラフでの深さ優先探索

--- src/Graph/GeneralGraph.py
+++ src/Graph/GeneralGraph.py
@@ -1,7 +1,10 @@
+"""単純無向グラフの実装群"""
 from collections import defaultdict
 from sys import stdin
 
 class DFS:
+    """単純無向グラフでの深さ優先探索
+    """    
     def __init__(self):
         N, M = map(int,stdin.readline().split())
         self._d = defaultdict(list)
@@ -12,5 +15,22 @@ class DFS:
 
         self._visited = [False]*N
 
+    def explore(self, base = 0):
+        if not self._visited[base]:
+            self._visited[base] = True
+            for idx in self._d[base]:
+                self.explore(idx)
+
+    @property
     def d(self):
         return self._d
+
+    @property
+    def visited(self):
+        return self._visited
+
+    def unreached_num(self):
+        return len(self._visited) - sum(self._visited)
+
+    def reached_num(self):
+        return len(self._visited) - sum(self._visited)
